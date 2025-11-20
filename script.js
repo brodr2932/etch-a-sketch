67d34ac028075625197bc16e21f215641dcf94f5
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-20 15:24:20 -0800

    Rewrite for loop to use innerHTML instead

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const container = document.querySelector(".container");
 
 // Creates 256 divs
 for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  container.appendChild(square);
+  container.innerHTML += '<div class="square"></div>';
 }
