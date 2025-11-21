b4d6c2af741abdeecd66744e205761dcf09a128e
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-21 15:00:30 -0800

    Declare function to change grid size

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 
-// Creates 256 squares
+// Creates 256 squares for initial grid size
 for (let i = 0; i < 256; i++) {
   container.innerHTML += '<div class="square"></div>';
 }
@@ -10,3 +10,15 @@ const squares = document.querySelectorAll(".square");
 squares.forEach(square => {
   square.addEventListener('mouseover', () => square.style.backgroundColor = "black");
 })
+
+
+const gridSizeBtn = document.querySelector('#grid-size-btn');
+
+gridSizeBtn.addEventListener('click', () => {
+  let userInput = parseInt(prompt('Enter the number of squares per side (e.g., 20 for a 20×20 grid):'));
+  changeGridSize(userInput);
+});
+
+function changeGridSize(numSquaresPerSide) {
+  
+}
