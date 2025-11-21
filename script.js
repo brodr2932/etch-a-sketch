6c3873babd6bf35486071120103d069356c569f3
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-21 13:52:38 -0800

    Apply hover effect to idividual squares

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const container = document.querySelector(".container");
 
-// Creates 256 divs
+// Creates 256 squares
 for (let i = 0; i < 256; i++) {
   container.innerHTML += '<div class="square"></div>';
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(square => {
+  square.addEventListener('mouseover', () => square.style.backgroundColor = "black");
+})
